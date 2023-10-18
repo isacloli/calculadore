22724e04c17e48cb5d21aea39d867abd5287c89b
isacloli <135846785+isacloli@users.noreply.github.com>
2023-10-18 10:50:02 -0300

    sla

---
+++ main.js
@@ -0,0 +1,10 @@
+//main.js
+
+let display = document.getElementById ("display");
+let currentlnput = "";
+
+function appendToDisplay(value) {
+    currentlnput += value;
+    display.valeu = currentlnput;
+}
+
